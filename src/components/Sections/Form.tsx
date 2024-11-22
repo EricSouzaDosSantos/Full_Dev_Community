cdf9dcbff1acac8a9b153fe286650465fcd063af
Gustavo Gantois <gugagantois@gmail.com>
2024-11-22 16:56:09 -0300

    fix: Enviando a esturtura do Login form para o projeto Builder.form

--- src/components/Sections/Form.tsx
+++ src/components/Sections/Form.tsx
@@ -1,7 +1,17 @@
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
+
 export function Form() {
     return (
         <form action="">
-            
+            <div className="flex flex-col">
+            <Label>Email</Label>
+            <Input placeholder="Digite seu email" />
+            </div>
+            <div className="flex flex-col">
+                <Label>Senha</Label>
+                <Input placeholder="Digite sua senha" />
+            </div>
         </form>
     )
 }
