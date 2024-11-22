de38654c45eb19d3fb6ee82d5064f15843b590bc
Gustavo Gantois <gugagantois@gmail.com>
2024-11-22 17:48:42 -0300

    feat: Criando a primeira página da aplicação. Página de Login

--- src/components/Sections/Form.tsx
+++ src/components/Sections/Form.tsx
@@ -1,16 +1,25 @@
+import { Button } from "../ui/button";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
 
 export function Form() {
     return (
-        <form action="">
+        <form action="" className="mt-4 lg:mt-8 flex flex-col justify-center items-center gap-4">
             <div className="flex flex-col">
-            <Label>Email</Label>
-            <Input placeholder="Digite seu email" />
+            <Label className="mb-2 lg:mb-0">Email</Label>
+            <Input type="email" placeholder="Digite seu email" />
             </div>
             <div className="flex flex-col">
-                <Label>Senha</Label>
-                <Input placeholder="Digite sua senha" />
+                <Label className="mb-2 lg:mb-0">Senha</Label>
+                <Input type="password" className="" placeholder="Digite sua senha" />
+            </div>
+            <div className="flex gap-12 lg:gap-48 mb-4">
+                <a href="/register" className="text-gray-400 text-sm">Lembrar de mim</a>
+                <a href="/register" className="text-gray-400 text-sm duration-300 transition-all hover:text-orange-700">Esqueci a senha</a>
+            </div>
+            <div className="flex justify-center items-center flex-col">
+                <Button variant="fulldev">Entrar</Button>
+                <a href="/register" className="text-gray-500 mt-4">Não tem uma conta? <span className="text-black font-semibold duration-300 transition-all hover:text-orange-700">Crie agora uma</span></a>
             </div>
         </form>
     )
--- src/components/Sections/Login.tsx
+++ src/components/Sections/Login.tsx
@@ -1,19 +1,28 @@
 import Image from "next/image";
 import { Form } from "./Form";
+import { Button } from "../ui/button";
+import { FaFacebook, FaGoogle } from 'react-icons/fa'
 
 export function Login() {
     return (
         <section className="grid lg:grid-cols-2">
-            <div className="bg-[#B22B00] w-full lg:h-screen">
-                <div className="mt-20 mb-20 lg:mb-0 lg:mt-56 flex justify-center">
-                    <Image src="/LogoWhite.svg" width={300} height={300} alt="" />
+            <div className="bg-[#B22B00] w-full lg:h-screen hidden lg:flex lg:justify-center lg:items-center">
+                <div className="mt-20 mb-20 lg:mb-0 lg:mt-32 flex justify-center">
+                    <Image src="/LogoWhite.svg" width={300} height={300} alt="" className="" />
                 </div>
             </div>
-            <div className="mt-20 lg:mt-40">
+            <div className="flex flex-col justify-center items-center mt-24 lg:mt-0">
                 <h1 className="text-center text-2xl lg:text-4xl font-bold">Bem vindo de volta</h1>
-                <p className="text-gray-500 text-center text-xl">Entre para começar a criar seus formulários.</p>
+                <p className="text-gray-500 text-center lg:text-xl mt-2">Entre para começar a criar seus formulários.</p>
+                <div>
+                <Form />
+                </div>
+                <p className="text-gray-300 mt-5 hidden lg:flex">Ou</p>
+                <div className="flex flex-col gap-4 mt-6">
+                <Button variant="outline"><FaGoogle size={16} className="text-orange-500"  /> Entrar usando a conta do Google</Button>
+                <Button variant="outline"><FaFacebook size={16} className="text-[#1877F2]" /> Entrar usando a conta do Facebook</Button>
+                </div>
             </div>
-            <Form />
         </section>
     )
 }
