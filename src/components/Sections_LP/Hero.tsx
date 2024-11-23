6f1d25ab12f0e9dff0638f90f6e438a5bbbfc610
Gustavo Gantois <gugagantois@gmail.com>
2024-11-22 21:56:28 -0300

    style:Adicionando uma nova imagem ao projeto

--- src/components/Sections_LP/Hero.tsx
+++ src/components/Sections_LP/Hero.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 export function Hero() {
     return (
-        <section>
+        <section className="overflow-hidden">
             <div className="flex justify-center flex-col items-center mt-40">
                 <h1 className="text-center font-semibold text-2xl lg:text-4xl text-white" data-aos="flip-up">
                 <div className="flex gap-2 justify-center items-center mb-2 animate-bounce">
@@ -26,6 +26,9 @@ export function Hero() {
                 <div className="mt-10">
                     <Image src="/ImgHero.svg" width={800} height={400} priority alt="" className="animate-float" />
                 </div>
+            </div>
+            <div className="mt-40 mb-40 flex justify-center items-center" data-aos="zoom-out">
+                <Image src="/ProgressBarImage.png" width={800} height={200} alt="" />
             </div>
         </section>
     )
