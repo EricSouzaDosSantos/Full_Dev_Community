774f531c93b770a90c28a05d41508c68c68395ac
Gustavo Gantois <gugagantois@gmail.com>
2024-11-22 21:51:48 -0300

    style:Mudando a logo do projeto

--- src/components/Sections_LP/Header.tsx
+++ src/components/Sections_LP/Header.tsx
@@ -8,7 +8,7 @@ export function Header() {
                 <p className="text-white text-center animate-pulse">Construa o seu formulário agora com o <span className="font-bold">Grupo 11</span></p>
             </div>
             <nav className="max-w-[1800px] mx-auto mt-6 flex justify-between">
-                <Image src="/logoHeader.svg" width={150} height={300} alt="" data-aos="fade-down" />
+                <Image src="/logoHeader.png" width={150} height={300} alt="" data-aos="fade-down" />
                 <a href="/register" data-aos="fade-down" data-aos-delay="200">
                     <Button>Inscrever-se</Button>
                 </a>
